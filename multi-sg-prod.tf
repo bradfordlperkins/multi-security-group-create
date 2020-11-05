32376d5f5d587df325d1e3b59b88e73dc40d8f24
bradfordlperkins <56170267+bradfordlperkins@users.noreply.github.com>
2020-11-05 14:40:08 -0700

    Update multi-sg-prod.tf

--- multi-sg-prod.tf
+++ multi-sg-prod.tf
@@ -10,6 +10,17 @@ resource "aws_security_group" "test_sg_new_1" {
   dynamic "ingress" {
     for_each = ["10.116.248.0/23","10.117.0.0/16","10.119.0.0/16","10.33.176.0/22","10.41.160.0/22","10.116.84.0/22","10.118.84.0/22"]
     content {
+      from_port   = 1234
+      to_port     = 1234
+      protocol    = "tcp"
+      cidr_blocks = [ingress.value]
+      description = "gms GenericServer"
+    }
+  }
+
+  dynamic "ingress" {
+    for_each = ["10.116.248.0/23","10.117.0.0/16","10.119.0.0/16","10.33.176.0/22","10.41.160.0/22","10.116.84.0/22","10.118.84.0/22"]
+    content {
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
@@ -235,6 +246,17 @@ resource "aws_security_group" "test_sg_new_1" {
       protocol    = "tcp"
       cidr_blocks = [ingress.value]
       description = "mrcp asr ResourceAccessPoint"
+    }
+  }
+
+  dynamic "ingress" {
+    for_each = ["10.116.248.0/23","10.117.0.0/16","10.119.0.0/16","10.33.176.0/22","10.41.160.0/22","10.116.84.0/22","10.118.84.0/22"]
+    content {
+      from_port   = 8010
+      to_port     = 8010
+      protocol    = "tcp"
+      cidr_blocks = [ingress.value]
+      description = "wfm server etl WFMServer"
     }
   }
 }
