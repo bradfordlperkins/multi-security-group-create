350b46a97542b529a90be34e52de054ccb25d6e5
bradfordlperkins <56170267+bradfordlperkins@users.noreply.github.com>
2020-11-05 15:04:56 -0700

    Update multi-sg-prod.tf

--- multi-sg-prod.tf
+++ multi-sg-prod.tf
@@ -250,6 +250,17 @@ resource "aws_security_group" "test_sg_new_1" {
   }
 
   dynamic "ingress" {
+    for_each = ["10.116.248.0/23","10.117.0.0/16","10.119.0.0/16","10.33.176.0/22","10.41.160.0/22","10.116.84.0/22","10.118.84.0/22"]
+    content {
+      from_port   = 8010
+      to_port     = 8010
+      protocol    = "tcp"
+      cidr_blocks = [ingress.value]
+      description = "wfm server etl WFMServer"
+    }
+  }
+
+  dynamic "ingress" {
     for_each = ["10.67.0.0/16","10.68.0.0/16"]
     content {
       from_port   = 0
