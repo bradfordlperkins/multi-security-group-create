41ebbcfc4bf71dfd9cfcba4ba054a3fe83acf83e
bradfordlperkins <56170267+bradfordlperkins@users.noreply.github.com>
2020-11-13 14:08:00 -0700

    Update multi-sg-prod.tf

--- multi-sg-prod.tf
+++ multi-sg-prod.tf
@@ -29,16 +29,27 @@ resource "aws_security_group" "test_sg_new_4" {
     }
   }
 
-  dynamic "ingress" {
-    for_each = ["10.117.0.0/16","10.119.0.0/16","10.33.176.0/22","10.41.160.0/22"]
-    content {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = [ingress.value]
-      description = "Workspace HTTP"
+    dynamic "ingress" {
+        for_each = ["10.117.0.0/16","10.119.0.0/16","10.33.176.0/22","10.41.160.0/22"]
+        content {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = [ingress.value]
+        description = "Workspace HTTP"
+        }
     }
-  }
+
+    dynamic "ingress" {
+        for_each = ["10.117.0.0/16","10.119.0.0/16","10.33.176.0/22","10.41.160.0/22"]
+        content {
+        from_port   = 9443
+        to_port     = 9443
+        protocol    = "tcp"
+        cidr_blocks = [ingress.value]
+        description = "Workspace Config Proxy"
+        }
+    }    
 
   dynamic "ingress" {
     for_each = ["10.117.0.0/16","10.119.0.0/16","10.33.176.0/22","10.41.160.0/22"]
